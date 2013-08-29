5faff3b6e5afd90072506cc04e3931304c10a07f
paulogaspari <paulogaspar@me.com>
2013-08-29 14:12:15 +0100

    first real solution working

--- project_anagram.rb
+++ project_anagram.rb
@@ -10,22 +10,35 @@
 ###########################################################
 
 
-check_for_anagrams = ['stars', 'mary', 'rats', 'tars', 'army']
-
-
+check_for_anagrams = ['mary', 'rats', 'tars', 'army', 'asdasdasdasd']
 anagram_groups = [] # create a container
+
+
+
+
+
+
+
 check_for_anagrams.each do |word| #  #=> stars
   anagrams = []
   # anagrams << word
   check_for_anagrams.each do |another_word| #=> mary
   	# check if they are anagrams
+
+  	if another_word != word && another_word.chars.sort == word.chars.sort 
+  		anagrams << word
+  	else
+  		
+
+  	end
   	# if so, put it into anagrams array
   end
+
+
   anagram_groups << anagrams
 end
 
-puts anagram_groups
+print anagram_groups
 
 # output
 
- 
