e8f10f4d7ea4f60a5167850873e38fb6031d5b17
paulogaspari <paulogaspar@me.com>
2013-08-29 15:15:20 +0100

    initiating rspec tests

--- project_anagram_v2.rb
+++ project_anagram_v2.rb
@@ -11,12 +11,23 @@
 
 
 check_for_anagrams = ['stars', 'mary', 'rats', 'tars', 'army', 'banana']
-anagram_groups = [] # create a container
+anagram_output = [] # create a container
 
 
+check_for_anagrams.each do |first_pass|
 
+	inside_arrays = []
 
+	check_for_anagrams.each do |second_pass|
 
+		if first_pass.chars.sort == second_pass.chars.sort
+			inside_arrays << second_pass
+		end
 
+	end
+anagram_output << inside_arrays
+end
 
+anagram_output.uniq!
 
+print anagram_output
