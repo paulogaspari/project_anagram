5344a70c159e56bfd379ea37e92030a675e32433
paulogaspari <paulogaspar@me.com>
2013-08-29 11:25:53 +0100

    No real progress

--- project_anagram.rb
+++ project_anagram.rb
@@ -1,4 +1,24 @@
-class Array 
-	def anagrammer
-	end
-end
+###########################################################
+# PROBLEM
+# 
+# Create a container
+# for every word:
+# 	- put it in a container
+# 	- compare to every other word to see if they are anagrams
+# 	- if yes add to the container
+# 
+###########################################################
+
+
+check_for_anagrams = ['stars', 'mary', 'rats', 'tars', 'army']
+
+
+decomposing_the_string = check_for_anagrams.repeated_permutation(1).to_a
+# or
+# decomposing_the_string = check_for_anagrams.combination(1).to_a
+
+
+decomposing_the_string.each {|x| x.str.each_char.to_a}
+
+
+	print decomposing_the_string
--- project_anagram_spec.rb
+++ project_anagram_spec.rb
@@ -1,3 +1,17 @@
 require './project_anagram'
 
-describe 'Anagramco'
+describe 'ProjectAnagram' do
+
+	let(:anagramy) { ProjectAnagram.new }
+	
+	it "should have every word on a container" do
+		anagramy.should 
+	end
+
+
+
+
+end 
+
+
+check_for_anagrams = [['stars'], ['mary'], ['rats'], ['tars'], ['army']
